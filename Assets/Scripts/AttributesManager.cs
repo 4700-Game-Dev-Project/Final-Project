fecb8530450b53aeab73d400b4813af092d5434d
BrandonDo211 <74513394+BrandonDo211@users.noreply.github.com>
2023-05-03 16:19:15 -0700

    powerup changes part2
    hi

--- Assets/Scripts/AttributesManager.cs
+++ Assets/Scripts/AttributesManager.cs
@@ -6,6 +6,9 @@ public class AttributesManager : MonoBehaviour
 {
     public int health;
     public int attack;
+    public float speed;
+    public int healAmount;
+
 
     public void TakeDamage(int amount)
     {
@@ -22,6 +25,60 @@ public class AttributesManager : MonoBehaviour
         }
     }
 
+    public void AddHealthToTarget(GameObject target)
+    {
+        var atm = target.GetComponent<AttributesManager>();
+        if(atm != null)
+        {
+            atm.addHealth(healAmount);
+        }
+        
+    }
+
+    public void addHealth(int amount)
+    {
+        if (health + amount > 100)
+        {
+            health = 100;
+            return;
+        }
+        health += amount;
+    }
+
+    public void setHealthToTarget(GameObject target)
+    {
+        var atm = target.GetComponent<AttributesManager>();
+        if (atm != null)
+        {
+            atm.setHealth(healAmount);
+        }
+    }
+    
+    public void setHealth(int amount)
+    {
+        health = amount;
+    }
+
+    public void setSpeedToTarget(GameObject target, int s)
+    {
+        var atm = target.GetComponent<AttributesManager>();
+        if (atm != null)
+        {
+            atm.setSpeed(healAmount);
+        }
+    }
+
+    public void setSpeed(int s)
+    {
+        speed = s;
+    }
+
+
+
+    public float GetSpeed()
+    {
+        return speed;
+    }
     public int GetHealth()
     {
         return health;
